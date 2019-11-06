e0bb73383768e918dda5f87f7ef710d35c19e94b
KVasilas <kcvasilas@gmail.com>
2019-11-05 23:02:26 -0500

    nomenclature

--- density_monitor/check_pop.sh
+++ density_monitor/check_pop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-nmap command >> RAW
 
 for ((i = 0 ; i < 168 ; i++)); do
   echo "traceroute $i"
-    nmao words >> RAW
-    awk -f user_parse.awk RAW >> number_users
-    #awk -f name_mac.awk TraceData >> numbers
+    nmap words >> RAW
+    awk -f host_parse.awk RAW >> number_host
+    awk -f ip_parse.awk RAW >> number_ip
+    #awk -f name_mac.awk TraceData >> mac_name
     sleep 60
 
 done
